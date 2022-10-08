d3b172739eb1aab8096cc25d272cfb83da2dee8e
Ashish Kumar <97027791+Ashish020202@users.noreply.github.com>
2022-10-08 19:45:48 +0530

    Initial commit

---
+++ src/component/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Kudos to Title</h1>
+      <p>Here Note content</p>
+    </div>
+  );
+}
+export default Note;
